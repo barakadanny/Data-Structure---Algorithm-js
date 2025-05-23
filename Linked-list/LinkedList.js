e30d608c0e6fc37a8d7be37338be69e975812a91
barakadanny <barakadan421@gmail.com>
2025-05-23 17:28:57 +0200

    unshift/ Linked List

--- Linked-list/LinkedList.js
+++ Linked-list/LinkedList.js
@@ -45,4 +45,17 @@ class LinkedList {
     }
     return current;
   }
+
+  unshift(value) {
+    let newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
